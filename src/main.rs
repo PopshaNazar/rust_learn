be87b2bc6c0c9c0ebf37b70e5d411f9eea700fc6
PopshaNazar <nazarpopsha@gmail.com>
2024-10-20 21:36:30 +0300

    3-6

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,17 @@ fn main() {
     let (s2, len) = calculate_length(s1);
 
     println!("The length of '{}' is {}.", s2, len);
+
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
 }
 
 fn calculate_length(s: String) -> (String, usize) {
